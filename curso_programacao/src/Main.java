5de1281d5dc36300a58e73a3eb4421acc4d183b2
Lucas Lopes <lucaslopeschaves1997@gmail.com>
2024-12-13 10:13:17 -0300

    Calculo imc

--- curso_programacao/src/Main.java
+++ curso_programacao/src/Main.java
@@ -4,42 +4,75 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		String nome;
-		int idade ;
-		double altura ;
-		
-		
-	
+		int idade, peso;
+		double altura;
+		double resultado;
+
 		Scanner sc = new Scanner(System.in);
-		sc.useLocale(Locale.US);
-		
-		// Entrada de nome 
-		System.out.println("Diga seu nome por favor :");
+		Locale.setDefault(Locale.US);
+
+		// Entrada de nome
+		System.out.println("Diga seu nome por favor:");
 		nome = sc.nextLine();
-		
-		// Entrada de idade 
-		System.out.println("agora sua idade");
+
+		// Entrada de idade
+		System.out.println("Sua idade:");
 		idade = sc.nextInt();
-		
-		// Limpeza de buffer 
-		sc.nextLine();	
-		
+
+		// Entrada peso
+
+		System.out.println("Seu peso:");
+		peso = sc.nextInt();
+
+		// Limpeza de buffer
+		sc.nextLine();
+
 		// Entrada de altura
 		System.out.println("altura:");
 		altura = sc.nextDouble();
+
+		// operação IMC
+
+		resultado = (double) peso / (altura * altura);
+
+		// Exibiçao de dados
+		System.out.printf("%s, voce está pesando %d kg e tem %.2f metros de altura.%n",nome,peso,altura);
+
+		System.out.println();
+
+		System.out.printf("O seu IMC é : %.2f%n ", resultado);
+
+		// estrutura condicional
 			
-		
-		// Exibiçao de dados	
-		System.out.println("seu nome é " + nome);
-		System.out.println ("voce tem " + idade + "anos");
-		System.out.printf("e mede %.2f metros.\n", altura);
-		
-		
+		if (resultado < 18.5) {
+
+			System.out.println("Abaixo do peso Procure se alimentar melhor");
+		}
+		else if (resultado < 24.9) {
+			
+			System.out.println("Peso normal Parabéns");
+		}
+		else if (resultado < 29.9) {
+			
+			System.out.println("Pré obesidade Procure um medico !!!");
+		}
+		else if (resultado < 34.9 ) {
+			
+			System.out.println("Obeso I Procure um medico !!!");
+		}
+		else if (resultado < 39.9) {
+			
+			System.out.println("Obeso II Procure um medico !!!");
+		} else {  
+			
+			System.out.println("obeso III Procure um medico !!!");
+		}
 		
 		// fechando scanner
 		sc.close();
-		
+
 	}
 
 }
